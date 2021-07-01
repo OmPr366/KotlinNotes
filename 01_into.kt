f41b31e7ddf9fe465625b0b8668df224a89eecef
OmPrakash <ompra.rox@gmail.com>
2021-07-01 23:58:24 +0530

    Till Operator

---
+++ 01_into.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+/*  What is Kotlin?
+-> New Programming language by Jetbrains (MODERN replacement of Java )
+	JetBrains devloped softwares like Android STUDIO, Intellij Idea m Pycharm etc.
+	Target JVM, Abdroid , Native & JavaScript.
+
+HISTORY OF KOTLIN 
+	> In 2011, JetBrains announced the devlopment of kotlin
+	> Made it open source
+	> 1.0 was live in 2016
+	> Google IO 2017, Andriod's first class support for kotlin.
+	> In 2019, Android is now Kotlin First.
+
+FEATURES OF KOTLIN
+	> Statically Typed Language( Check during compile time )
+	> Object Oriented and Functional Language
+	> 100% interoperable with Java (we can use both Java & Kotlin in Same project )
+	> Concise, Safe and Powerful
+
+*/
+
+}
---
+++ 02_var.kt
@@ -0,0 +1,29 @@
+fun main() {
+    println("Myself Om Prakash")
+    println("Myself Om Prakash")
+    print(1+2)
+    /* Two Types of variable 
+      :- 1. Var  & 2. Val
+      	Var can be changed but Val can't be changed (work as a const)
+        */
+    println(false)
+    var a = 71111
+    println(a)
+    val b = 89
+    println(b)
+    //b = 897     This is Wrong
+    println(a)
+    
+    // While assigning float in a it gives error (Type checking)
+    // var a = 8.1    (Error  We can't assign float 
+    // 
+    
+    
+    // WE can assign variable a:-
+    var num : Int = 9876
+    var avg : Double = 9887.9
+    var check : Boolean = false
+    var favChar : char = 'A';
+    var name : String = "Name";
+    println(check)
+}
