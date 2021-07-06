5646e6132332be924b59a5ea61220ee0fec2c340
OmPrakash <ompra.rox@gmail.com>
2021-07-06 15:36:53 +0530

    Till List

--- Codes/src/main/kotlin/32_list.kt
+++ Codes/src/main/kotlin/32_list.kt
@@ -3,6 +3,21 @@ fun main() {
      1. Mutable List (Which can be changed)
      2. Immutable list  (Which can't be changed)
      */
-    val num  = listOf<Int>(2,3,4,5,6)
-    println(num.indexOf(3))
+    // Mutable List
+    val num  = mutableListOf<Int>(2,3,4,5,6)
+    println(num.indexOf(6))
+    println(num.contains(4))
+    println(num.contains(1))
+    num[4] = 78
+
+    num.add(5)
+    num.remove(2)
+    println(num)
+    num.removeAt(2)
+    println(num)
+
+
+    val num2 = mutableListOf<Int>(11,22,33)
+    num2.addAll(num)
+    println("Here is Lis2:- $num2")
 }
